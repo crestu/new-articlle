ec40b7e9e087922135bbfd1f5a630e70c15b48cb
Creed Studenski <creedstudenski@Creeds-MacBook-Air.local>
2023-11-11 16:26:54 -0600

    category update and and delete functions

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,20 +1,66 @@
 class CategoriesController < ApplicationController
-    # before_action :set_category, only: [:show, :edit, :update, :destroy]
+    before_action :set_category, only: [:show, :destroy]
     
     def index
       @categories = Category.all
+
     end
   
-    # def show
-    #   # Display category details and associated articles
-    #   @articles = @category.articles
+    def show
+      # Display category details and associated articles
+      @articles = @category.articles
+    end
+
+    def new
+      @category = Category.new
+    end
+
+
+    def create
+      @category = Category.new(categories_params)
+
+
+      if @category.save
+        redirect_to categories_path
+      else
+        render :new, status: :unprocessable_entity
+      end
+    end
+
+    def edit
+      @category = Category.find(params[:id])
+    end
+
+    def update
+      @category = Category.find(params[:id])
+
+      if @category.update(categories_params)
+        redirect_to categories_path, notice: 'Category was successfully updated.'
+      else
+        render :edit
+      end
+    end
+
+
+    def destroy
+      @category = Category.find(params[:id])
+      redirect_to root_path, status: :see_other
+       @category.destroy
+    end
+
+    # def destroy
+    #   @category.destroy
+    #   redirect_to category_articles_path(@category), notice: 'Category was successfully destroyed.'    
     # end
 
-    
-   
     private
   
     def set_category
       @category = Category.find(params[:id])
     end
+
+    def categories_params
+      params.require(:category).permit(:name, :description)
+    end
+
   end
